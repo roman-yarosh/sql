d02399fce5e0bbacc7a3ebdebd35f756bf289e1b
Roman Yarosh <yracompany@gmail.com>
2017-06-05 21:20:15 +0300

    2 task

--- 2.sql
+++ 2.sql
@@ -1,6 +1,6 @@
 select p.project_id, p.project_name, sum(d.salary) as project_cost from projects p
 inner join developers_projects dp on p.project_id = dp.project_id
 inner join developers d on dp.developer_id = d.developer_id
-group by p.project_id
+group by p.project_id, p.project_name
 order by project_cost desc
 limit 1;
