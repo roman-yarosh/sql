e91b8d758e10ff6c7794b6f600282073a223b9f0
Roman Yarosh <yracompany@gmail.com>
2017-06-05 21:24:26 +0300

    6 task

--- 6.sql
+++ 6.sql
@@ -1,4 +1,23 @@
+Old version:
+
 select project_name, cost, avg(salary) from projects p
 inner join developers d on p.project_id = d.project_id
 where cost = (select min(cost) min_cost from projects)
+group by project_name, cost
+
+New version:
+
+1 variant:
+select project_name, cost, avg(salary) avg_salary from projects p
+inner join developers_projects dp on p.project_id = dp.project_id
+inner join developers d on dp.developer_id = d.developer_id
+group by project_name, cost
+order by cost
+limit 1;
+
+2 variant:
+select project_name, cost, avg(salary) avg_salary from projects p
+inner join developers_projects dp on p.project_id = dp.project_id
+inner join developers d on dp.developer_id = d.developer_id
+where p.project_id = (select project_id from projects where cost = (select min(cost) from projects))
 group by project_name, cost
